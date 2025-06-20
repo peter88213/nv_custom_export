b5e61ebc64bd24cf26acfeeb282ffa78bae5f8fc
peter88213 <petert88213@gmail.com>
2025-06-20 09:51:58 +0200

    Updated docstrings.

--- src/nvcustomexport/custom_export_service.py
+++ src/nvcustomexport/custom_export_service.py
@@ -1,7 +1,7 @@
 """Provide a class for the custom template-based export service.
 
 Copyright (c) 2025 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/nv_custom_export
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
